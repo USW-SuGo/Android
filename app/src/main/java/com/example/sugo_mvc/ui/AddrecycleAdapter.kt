1bd58c6da8880dde14f0b5bea4ffff2e2be11576
lsakee <lsls4868@gmail.com>
2022-10-10 21:08:57 +0900

    Feat Api

--- app/src/main/java/com/example/sugo_mvc/ui/AddrecycleAdapter.kt
+++ app/src/main/java/com/example/sugo_mvc/ui/AddrecycleAdapter.kt
@@ -17,23 +17,37 @@ import kotlinx.coroutines.withContext
 
 class AddrecycleAdapter(private val items : ArrayList<Uri>,val context: Context) :
     RecyclerView.Adapter<AddrecycleAdapter.ViewHolder>() {
-
+    private lateinit var itemClickListener : OnItemClickListener
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflatedView =LayoutInflater.from(parent.context).inflate(R.layout.addsugorecycleitem,parent,false)
         return ViewHolder(inflatedView)
+
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = items[position]
-        Glide.with(context)
-            .load(item)
-            .override(500,500)
-            .into(holder.image)
+            items[position].let { items ->
+                with(holder.itemView){
+                    Glide.with(context)
+                        .load(items)
+                        .override(500,500)
+                        .into(holder.image)
+                }
+
+            }
+
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
+
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+    fun setItemClickListener(itemClickListener: OnItemClickListener) {
+        this.itemClickListener = itemClickListener
+    }
+
     class ViewHolder(v:View) : RecyclerView.ViewHolder(v){
         private var view:View = v
         var image = v.findViewById<ImageView>(R.id.addrecycleitem)
