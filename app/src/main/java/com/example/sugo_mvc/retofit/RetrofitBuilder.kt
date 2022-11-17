4ac6ffdb709a7d8ede7c56ef2dc91a2394f73e53
이진욱 <koownij@kakao.com>
2022-11-18 00:10:49 +0900

    fix: AuthInterceptor가 잘못된 Authorization Header로 교체하는 문제 파악 및 임시 조치

--- app/src/main/java/com/example/sugo_mvc/retofit/RetrofitBuilder.kt
+++ app/src/main/java/com/example/sugo_mvc/retofit/RetrofitBuilder.kt
@@ -1,5 +1,6 @@
 package com.example.sugo_mvc.retofit
 
+import android.util.Log
 import com.example.sugo_mvc.util.App
 import com.example.sugo_mvc.util.Constants.Companion.BASE_URL
 import com.google.gson.*
@@ -7,6 +8,8 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONArray
+import org.json.JSONObject
 import org.threeten.bp.DayOfWeek.FROM
 import org.threeten.bp.LocalDate.FROM
 import retrofit2.Retrofit
@@ -18,31 +21,53 @@ import java.time.temporal.TemporalAccessor
 
 
 object RetrofitBuilder {
-    var service :SuRetrofit
-    var clientBuilder = OkHttpClient.Builder()
-    var loggingInterceptor = HttpLoggingInterceptor()
-    val okHttpClient = OkHttpClient.Builder().addInterceptor(AuthInterceptor()).build()
-    init{
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
-    val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .client(okHttpClient)
-        .addConverterFactory(NullOnEmptyConverterFactory())
-        .addConverterFactory(GsonConverterFactory.create())
+    var service: SuRetrofit
+
+    val okHttpClient = OkHttpClient.Builder()
+        //.addInterceptor(AuthInterceptor())
+        .addInterceptor(getLogOkHttpClient())
         .build()
 
-     service = retrofit.create(SuRetrofit::class.java)
-}
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(NullOnEmptyConverterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        service = retrofit.create(SuRetrofit::class.java)
+    }
+
+    private fun getLogOkHttpClient(): Interceptor {
+        val interceptor = HttpLoggingInterceptor { message ->
+            when {
+                message.isJsonObject() ->
+                    Log.d("Retrofit2", JSONObject(message).toString(4))
+                message.isJsonArray() ->
+                    Log.d("Retrofit2", JSONArray(message).toString(4))
+                else ->
+                    Log.d("Retrofit2", "CONNECTION INFO -> $message")
+            }
+        }
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
+    }
 
     class AuthInterceptor : Interceptor {
         override fun intercept(chain: Interceptor.Chain): Response {
             var req =
 
-                chain.request().newBuilder().addHeader("Authorization", App.prefs.AccessToken ?: "").build()
-                chain.request().newBuilder().addHeader("Authorization", App.prefs.RefreshToken ?: "").build()
+                chain.request().newBuilder().addHeader("Authorization", App.prefs.AccessToken ?: "")
+                    .build()
+            chain.request().newBuilder().addHeader("Authorization", App.prefs.RefreshToken ?: "")
+                .build()
 
             return chain.proceed(req)
         }
     }
 
 }
+
+fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
+fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
