e9d0c55b9919e32ac619d048e86b33300f95ad9d
lsakee <lsls4868@gmail.com>
2022-11-19 19:32:50 +0900

    Feat LocalDateTimeGson format

--- app/src/main/java/com/example/sugo_mvc/retofit/RetrofitBuilder.kt
+++ app/src/main/java/com/example/sugo_mvc/retofit/RetrofitBuilder.kt
@@ -3,21 +3,23 @@ package com.example.sugo_mvc.retofit
 import android.util.Log
 import com.example.sugo_mvc.util.App
 import com.example.sugo_mvc.util.Constants.Companion.BASE_URL
-import com.google.gson.*
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import org.json.JSONArray
 import org.json.JSONObject
-import org.threeten.bp.DayOfWeek.FROM
-import org.threeten.bp.LocalDate.FROM
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.reflect.Type
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
-import java.time.temporal.TemporalAccessor
 
 
 object RetrofitBuilder {
@@ -29,15 +31,34 @@ object RetrofitBuilder {
         .build()
 
     init {
+
+        val gson = GsonBuilder()
+            .registerTypeAdapter(
+                LocalDateTime::class.java,
+                object : JsonDeserializer<LocalDateTime> {
+                    override fun deserialize(
+                        json: JsonElement?,
+                        typeOfT: Type?,
+                        context: JsonDeserializationContext?
+                    ): LocalDateTime {
+                        return LocalDateTime.parse(
+                            json?.asString,
+                            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+                        )
+                    }
+                }
+            )            .create()
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(okHttpClient)
             .addConverterFactory(NullOnEmptyConverterFactory())
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
         service = retrofit.create(SuRetrofit::class.java)
     }
+
+
 
     private fun getLogOkHttpClient(): Interceptor {
         val interceptor = HttpLoggingInterceptor { message ->
