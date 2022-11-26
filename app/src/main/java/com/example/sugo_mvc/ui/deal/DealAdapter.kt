03d0a4fbe077be06dab455ac434ffe7ca909d1c9
lsakee <lsls4868@gmail.com>
2022-11-26 17:16:51 +0900

    refactor : getMoreData

--- app/src/main/java/com/example/sugo_mvc/ui/deal/DealAdapter.kt
+++ app/src/main/java/com/example/sugo_mvc/ui/deal/DealAdapter.kt
@@ -12,14 +12,13 @@ import android.widget.Filterable
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+
 import com.example.sugo_mvc.data.DealMainItem
 import com.example.sugo_mvc.databinding.DealrvitemBinding
 import com.example.sugo_mvc.ui.DealDetailActivity
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
-class DealAdapter(val items: MutableList<DealMainItem>) :
+
+class DealAdapter(var items: MutableList<DealMainItem>) :
     RecyclerView.Adapter<DealAdapter.ViewHolder>(),Filterable {
     var filterItems= mutableListOf<DealMainItem>()
     var itemFilter= ItemFilter()
@@ -31,6 +30,14 @@ class DealAdapter(val items: MutableList<DealMainItem>) :
     }
     override fun getFilter(): Filter {
         return itemFilter
+    }
+    init {
+        filterItems=items
+//        filterItems.addAll(items)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = DealrvitemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
     class ViewHolder(binding: DealrvitemBinding) : RecyclerView.ViewHolder(binding.root){
         var binding: DealrvitemBinding
@@ -50,86 +57,75 @@ class DealAdapter(val items: MutableList<DealMainItem>) :
         init {
             this.binding = binding
             //item Click Listener
-            binding.root.setOnClickListener(View.OnClickListener {
+
+            binding.root.setOnClickListener({
                 val pos = adapterPosition
                 Log.d("click", pos.toString() + " : click!")
             })
         }
     }
-    init {
-        filterItems.addAll(items)
-    }
-    inner class LoadingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val progressBar = itemView.findViewById<ProgressBar>(R.id.rv_loading_pb)!!
-    }
 
     override fun getItemViewType(position: Int): Int {
-        return if (items[position] != null) {
+        return if (filterItems[position] != null) {
             ITEM
         } else {
             LOADING
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DealAdapter.ViewHolder {
-            val binding = DealrvitemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-           return ViewHolder(binding)
+
+
+
+    override fun getItemCount(): Int {
+        return filterItems.size
     }
-
-    interface ItemClick{ //인터페이스
-        fun onClick(view: View, position: Int)
-    }
-
-    var itemClick: ItemClick? = null
-
-    override fun onBindViewHolder(holder: DealAdapter.ViewHolder, position: Int) {
-        holder.bind(items!![position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(filterItems[position])
         holder.itemView.setOnClickListener{
 
-            val intent = Intent(holder.itemView?.context, DealDetailActivity()::class.java)
+            val intent = Intent(holder.itemView.context, DealDetailActivity()::class.java)
             intent.putExtra("id",   holder.binding.dealrvid.text)
             ContextCompat.startActivity(holder.itemView.context,intent,null)
         }
 
     }
-    override fun getItemCount(): Int {
-        return items.size
+    fun initFilteredPersons() {
+        filterItems.clear()
+        filterItems.addAll(items)
     }
-
-
     inner class ItemFilter : Filter() {
         override fun performFiltering(charSequence: CharSequence): FilterResults {
             val filterString = charSequence.toString()
             Log.d(TAG, "charSequence : $charSequence")
-            val results = FilterResults()
-            //검색이 필요없을 경우를 위해 원본 배열을 복제
-            val filteredList: MutableList<DealMainItem> = mutableListOf<DealMainItem>()
             //공백제외 아무런 값이 없을 경우 -> 원본 배열
-            if (filterString.trim { it <= ' ' }.isEmpty()) {
-                results.values = items
-                results.count = items.size
-                return results
+            if (filterString.isEmpty()) {
+                filterItems = items
+//                return results
             } else {
+                val resultList= mutableListOf<DealMainItem>()
                 for (item in items) {
-                    Log.d("search title",item.title.toString())
+                    Log.d("search title",item.title)
                     if (item.title.contains(filterString)) {
-                        filteredList.add(item)
-                        Log.d("searchfilter",filteredList.toString())
+                        resultList.add(item)
+                        Log.d("searchfilter",resultList.toString())
                     }
                 }
+                filterItems=resultList
             }
-            Log.d("searchfilter",filteredList.toString())
-            results.values = filteredList
-            results.count = filteredList.size
+            val results = FilterResults()
+            results.values=filterItems
+            Log.d("searchfilter2",results.values.toString())
+//            notifyDataSetChanged()
             return results
         }
 
+        @SuppressLint("NotifyDataSetChanged")
         override fun publishResults(p0: CharSequence?, p1: FilterResults) {
-            filterItems.clear()
-            filterItems.addAll(p1.values as MutableList<DealMainItem>)
+
+            filterItems=p1.values as MutableList<DealMainItem>
+            Log.d("searchfilter3",filterItems.toString())
             notifyDataSetChanged()
         }
-//        @SuppressLint("NotifyDataSetChanged")
 
     }
 }
