5f7331ef8262731c1051a307813a117871b30d80
lsakee <lsls4868@gmail.com>
2023-01-28 02:28:34 +0900

    feat main to deal / bottom navigation

---
+++ app/src/main/java/com/sugo/app/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sugo.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sugo.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.main_bottom_navigation)
+        bottomNavigationView.itemIconTintList=null
+        val navController = supportFragmentManager.findFragmentById(R.id.main_container)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
+    }
+}
--- app/src/main/java/com/sugo/app/ui/SplashActivity.kt
+++ app/src/main/java/com/sugo/app/ui/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.sugo.app
+package com.sugo.app.ui
 
 import android.content.Intent
 import android.os.Bundle
