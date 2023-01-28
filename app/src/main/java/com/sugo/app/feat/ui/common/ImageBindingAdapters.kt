83b40751f148fbc9a4b365653268de5f8bb2aac0
lsakee <lsls4868@gmail.com>
2023-01-28 09:09:00 +0900

    feat imagebindingadapter

---
+++ app/src/main/java/com/sugo/app/feat/ui/common/ImageBindingAdapters.kt
@@ -0,0 +1,16 @@
+package com.sugo.app.feat.ui.common
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view)
+            .load(imageUrl)
+            .into(view)
+    }
+}
+
+
