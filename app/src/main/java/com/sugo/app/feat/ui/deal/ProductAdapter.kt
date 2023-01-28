5245a130476bdc926282ab7b8abb97b995d21a64
lsakee <lsls4868@gmail.com>
2023-01-28 09:10:29 +0900

    feat dealProductAdapter

---
+++ app/src/main/java/com/sugo/app/feat/ui/deal/ProductAdapter.kt
@@ -0,0 +1,34 @@
+package com.sugo.app.feat.ui.deal
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sugo.app.databinding.ItemDealBinding
+import com.sugo.app.feat.model.DealProduct
+import com.sugo.app.feat.ui.common.CategoryDiffCallback
+
+class ProductAdapter(private val viewModel:DealProductViewModel) : ListAdapter<DealProduct, ProductAdapter.ProductViewHolder>(
+    CategoryDiffCallback()
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val binding = ItemDealBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ProductViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ProductViewHolder(private val binding: ItemDealBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(dealProduct: DealProduct){
+//            binding.viewModel = viewModel
+            binding.dealproduct=dealProduct
+            binding.executePendingBindings()
+        }
+    }
+
+}
