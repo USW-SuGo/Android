a9826fe16822496f562780b12e01f32f7557626f
lsakee <lsls4868@gmail.com>
2023-02-13 18:51:00 +0900

    feat getLikeProductFragment

--- app/src/main/java/com/sugo/app/feat/ui/mypage/MyPageAdapter.kt
+++ app/src/main/java/com/sugo/app/feat/ui/mypage/MyPageAdapter.kt
@@ -2,6 +2,7 @@ package com.sugo.app.feat.ui.mypage
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.sugo.app.databinding.ItemMypageBinding
@@ -9,12 +10,14 @@ import com.sugo.app.feat.model.DealProduct
 import com.sugo.app.feat.ui.common.DealProductDiffCallback
 import com.sugo.app.feat.ui.deal.ProductPagingViewModel
 
-class MyPageAdapter(private val viewModel: MyPageViewModel) : PagingDataAdapter<DealProduct, MyPageAdapter.PagingViewHolder>(
+class MyPageAdapter(
+    private val viewModel: MyPageViewModel,
+) : PagingDataAdapter<DealProduct, MyPageAdapter.PagingViewHolder>(
     DealProductDiffCallback()
 ) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagingViewHolder {
-        val binding = ItemMypageBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = ItemMypageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return PagingViewHolder(binding)
     }
 
@@ -25,10 +28,11 @@ class MyPageAdapter(private val viewModel: MyPageViewModel) : PagingDataAdapter<
     inner class PagingViewHolder(private val binding: ItemMypageBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(dealProduct: DealProduct){
-            binding.viewModel =viewModel
-            binding.dealproduct=dealProduct
+        fun bind(dealProduct: DealProduct) {
+            binding.viewModel = viewModel
+            binding.dealproduct = dealProduct
             binding.executePendingBindings()
+
         }
     }
 
--- app/src/main/java/com/sugo/app/feat/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sugo/app/feat/ui/mypage/MyPageFragment.kt
@@ -1,12 +1,17 @@
 package com.sugo.app.feat.ui.mypage
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.os.bundleOf
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -36,14 +41,33 @@ class MyPageFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setUser()
         val pagingAdapter = initAdapter()
-        productSubmitData(pagingAdapter)
+        //이부분을  VM 으로 옮기기 뷰엣허 클릭시 센변경을 하는방법을 모르곘네  셀렉터인가 아닌가?
+        binding.tvUserWrite.setOnClickListener {
+            productSubmitData(pagingAdapter)
+            binding.tvUserWrite.setTextColor(Color.parseColor("#000000"))
+            binding.tvLikeWrite.setTextColor(Color.parseColor("#419e3a"))
+        }
+        binding.tvLikeWrite.setOnClickListener {
+            productLikeSubmitData(pagingAdapter)
+            binding.tvUserWrite.setTextColor(Color.parseColor("#419e3a"))
+            binding.tvLikeWrite.setTextColor(Color.parseColor("#000000"))
+        }
+        openDailog()
         setNavigation()
     }
 
-    private fun initAdapter(): MyPageAdapter {
+    private fun openDailog() {
+        viewModel.openDealEvent1.observe(viewLifecycleOwner, EventObserver {
+            val bottomSheetFragment = CustomDialog(requireContext(), it)
+            Log.d("productPostId", it.toString())
+            bottomSheetFragment.show(childFragmentManager, "childFragmentManager")
+        })
+    }
 
+    private fun initAdapter(): MyPageAdapter {
         val pagingAdapter = MyPageAdapter(viewModel)
         binding.rvMypage.adapter = pagingAdapter
+        productSubmitData(pagingAdapter)
         return pagingAdapter
     }
 
@@ -73,6 +97,18 @@ class MyPageFragment : Fragment() {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 launch {
                     viewModel.getMyPageProduct().collectLatest { pagingData ->
+                        pagingAdapter.submitData(pagingData)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun productLikeSubmitData(pagingAdapter: MyPageAdapter) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.getLikeProduct().collectLatest { pagingData ->
                         pagingAdapter.submitData(pagingData)
                     }
                 }
