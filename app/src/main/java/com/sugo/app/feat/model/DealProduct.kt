ec5490c775e3c92b39b4a705cb21b99219261f83
lsakee <lsls4868@gmail.com>
2023-02-20 20:58:59 +0900

    feat 학과 데이터

--- app/src/main/java/com/sugo/app/feat/model/DealProduct.kt
+++ app/src/main/java/com/sugo/app/feat/model/DealProduct.kt
@@ -4,16 +4,18 @@ package com.sugo.app.feat.model
 import java.time.LocalDateTime
 
 import com.google.gson.annotations.SerializedName
+import kotlin.String
+
 data class DealProduct(
     @SerializedName("productPostId")val id: Long,
-    val imageLink:String,
+    val imageLink: String,
     val contactPlace: String,
     val updatedAt: LocalDateTime,
     val title: String,
     val price: Int,
     val nickname: String,
     val category: String,
-    val content:String,
+    val content: String,
     val status :Boolean,
 )
 data class ProductPostId(
---
+++ app/src/main/java/com/sugo/app/feat/model/Department.kt
@@ -0,0 +1,10 @@
+package com.sugo.app.feat.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Department(
+    @SerializedName("departments") val departments: ArrayList<String>
+)
+data class Depart(
+    val depart : String
+)
