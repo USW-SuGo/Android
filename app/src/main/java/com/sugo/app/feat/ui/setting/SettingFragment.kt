a3885bf1412f85bc6800e30d21816bd1814e9379
lsakee <lsls4868@gmail.com>
2023-03-01 17:28:50 +0900

    feat settingFragment Ui

---
+++ app/src/main/java/com/sugo/app/feat/ui/setting/SettingFragment.kt
@@ -0,0 +1,32 @@
+package com.sugo.app.feat.ui.setting
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sugo.app.databinding.FragmentMessageBinding
+import com.sugo.app.databinding.FragmentSettingBinding
+import com.sugo.app.feat.ui.login.LoginActivity
+
+class SettingFragment: Fragment() {
+
+    private lateinit var binding: FragmentSettingBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvLogincheck.setOnClickListener {
+            val nextIntent = Intent(activity, LoginActivity::class.java)
+            startActivity(nextIntent)
+        }
+    }
+}
