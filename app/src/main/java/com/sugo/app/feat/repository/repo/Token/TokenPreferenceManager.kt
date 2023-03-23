489697632acf2c80e3c220741127e987773849a7
issac <lsls4868@gmail.com>
2023-03-23 15:11:48 +0900

    feat FCM Token get/save

--- app/src/main/java/com/sugo/app/feat/repository/repo/Token/TokenPreferenceManager.kt
+++ app/src/main/java/com/sugo/app/feat/repository/repo/Token/TokenPreferenceManager.kt
@@ -27,15 +27,15 @@ class TokenPreferenceManager(context: Context) : TokenLocalDataSource {
     override fun saveRefreshToken(token: String?) {
         prefs.edit().putString(Refresh_TOKEN, token).apply()
     }
-    override fun savenote(token: String?) {
+    override fun saveFCM(token: String?) {
         prefs.edit().putString(TEST_NOTE, token).apply()
     }
-    override fun getnote(): String? {
+    override fun getFCM(): String? {
         return prefs.getString(TEST_NOTE, null)
     }
 
     private companion object {
-        const val TEST_NOTE="test"
+        const val TEST_NOTE="fcmtoken"
         const val PREFS_NAME = "preferences"
         const val ACCESS_TOKEN = "accesstoken"
         const val Refresh_TOKEN = "refreshtoken"
