3bf185b1f8bce8b1889ea1a41e1c1ab9e39516a0
issac <lsls4868@gmail.com>
2023-04-07 03:49:08 +0900

    refactor ui수정

--- app/src/main/java/com/sugo/app/feat/ui/upload/UploadActivity.kt
+++ app/src/main/java/com/sugo/app/feat/ui/upload/UploadActivity.kt
@@ -1,12 +1,17 @@
 package com.sugo.app.feat.ui.upload
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sugo.app.R
+import com.sugo.app.feat.ui.MainActivity
+import com.sugo.app.feat.ui.login.LoginActivity
 
 class UploadActivity: AppCompatActivity() {
 
