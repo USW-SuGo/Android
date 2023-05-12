7fcf561782447aac1694710c9774322ae2a7b5e6
issac <lsls4868@gmail.com>
2023-05-12 21:00:08 +0900

    feat : submitdata util

---
+++ app/src/main/java/com/sugo/app/feat/ui/common/SubmitData.kt
@@ -0,0 +1,29 @@
+package com.sugo.app.feat.ui.common
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.PagingData
+import androidx.paging.PagingDataAdapter
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+fun <T : Any> pagingSubmitData(
+    lifecycleOwner: LifecycleOwner,
+    getData: Flow<PagingData<T>>,
+    pagingAdapter: PagingDataAdapter<T, *>
+) {
+    lifecycleOwner.lifecycleScope.launch {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            launch {
+                getData.collectLatest { pagingData ->
+                    pagingAdapter.submitData(pagingData)
+                }
+            }
+        }
+    }
+}
